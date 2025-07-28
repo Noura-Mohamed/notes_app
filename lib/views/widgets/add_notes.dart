7ee6e4a9f729eddda37c6c9f82194bf00f9b70c1
Noura-Mohamed <noura.mohamed1032004@gmail.com>
2025-07-28 21:23:52 +0300

    Add custom text feild widget

--- lib/views/widgets/add_notes.dart
+++ lib/views/widgets/add_notes.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_text_feiled.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(height: 32,),
+          CustomTextFeiled(hint: 'Title'),
+          SizedBox(height: 16,),
+          CustomTextFeiled(hint: 'Content', maxLines: 5,),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/views/widgets/custom_text_feiled.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFeiled extends StatelessWidget {
+  const CustomTextFeiled({super.key, required this.hint,this.maxLines=1});
+
+  final String hint;
+  final int maxLines;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: Colors.cyanAccent,
+      maxLines: maxLines,
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: TextStyle(color: Colors.cyanAccent),
+        enabledBorder: buildBorder(color: Colors.white),
+        focusedBorder: buildBorder(color: Colors.cyanAccent),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder({color}) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: color),
+    );
+  }
+}
