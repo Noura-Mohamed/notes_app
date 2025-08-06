29242dccb9a3cd0abba0a7a9d6091091b9d56673
Noura-Mohamed <noura.mohamed1032004@gmail.com>
2025-08-06 17:04:24 +0300

    init hive and open notes box

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/views/notes_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('notes_box');
   runApp(const NotesApp());
 }
 
@@ -12,9 +15,7 @@ class NotesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Poppins',
-        brightness: Brightness.dark),
+      theme: ThemeData(fontFamily: 'Poppins', brightness: Brightness.dark),
       home: NotesView(),
     );
   }
