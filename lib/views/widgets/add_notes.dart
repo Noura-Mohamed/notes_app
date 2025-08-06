fad4025c7a28595465c5ae3de4b7fc0b46cd0aa0
Noura-Mohamed <noura.mohamed1032004@gmail.com>
2025-08-06 18:54:20 +0300

    take input and validate data

--- lib/views/widgets/add_notes.dart
+++ lib/views/widgets/add_notes.dart
@@ -9,21 +9,58 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 32,),
-            CustomTextFeiled(hint: 'Title'),
-            SizedBox(height: 16,),
-            CustomTextFeiled(hint: 'Content', maxLines: 5,),
-            SizedBox(height: 32,),
-            CustomButton(),
-            SizedBox(height: 32,),
-
-          ],
-        ),
-      ),
+      child: SingleChildScrollView(child: AddNoteForm()),
     );
   }
 }
 
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({super.key});
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          SizedBox(height: 32),
+          CustomTextFeiled(
+            onSaved: (value) {
+              title = value;
+            },
+            hint: 'Title',
+          ),
+          SizedBox(height: 16),
+          CustomTextFeiled(
+            onSaved: (value) {
+              subTitle = value;
+            },
+            hint: 'Content',
+            maxLines: 5,
+          ),
+          SizedBox(height: 32),
+          CustomButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          SizedBox(height: 32),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/widgets/custom_text_feiled.dart
+++ lib/views/widgets/custom_text_feiled.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
-
 class CustomTextFeiled extends StatelessWidget {
-  const CustomTextFeiled({super.key, required this.hint,this.maxLines=1});
+  const CustomTextFeiled({
+    super.key,
+    required this.hint,
+    this.maxLines = 1,
+    this.onSaved,
+  });
 
   final String hint;
   final int maxLines;
+  final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onSaved,
+      validator: (value) {
+        if (value?.isEmpty ?? true) {
+          return 'Field is required';
+        } else {
+          return null;
+        }
+      },
       cursorColor: Colors.cyanAccent,
       maxLines: maxLines,
       decoration: InputDecoration(
